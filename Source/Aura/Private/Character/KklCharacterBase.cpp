cd1d957cef5a817f1f2a2a5d56d5d6def27f4100
KazukoLzumi <2126142969@qq.com>
2025-12-04 19:52:07 +0800

    Characterz分类

---
+++ Source/Aura/Private/Character/KklCharacterBase.cpp
@@ -0,0 +1,17 @@
+
+
+#include "KklCharacterBase.h"
+
+AKklCharacterBase::AKklCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AKklCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Aura/Public/Character/KklCharacterBase.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "KklCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class AURA_API AKklCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AKklCharacterBase();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+};
