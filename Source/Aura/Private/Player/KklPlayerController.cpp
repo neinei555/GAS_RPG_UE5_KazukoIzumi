9af06fdfcadb18e98488fb1fd28e51c465171e86
KazukoLzumi <2126142969@qq.com>
2025-12-05 22:06:21 +0800

    移动输入绑定

--- Source/Aura/Private/Player/KklPlayerController.cpp
+++ Source/Aura/Private/Player/KklPlayerController.cpp
@@ -2,4 +2,55 @@
 
 
 #include "Player/KklPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
+
+AKklPlayerController::AKklPlayerController()
+{
+	bReplicates = true;
+}
+
+void AKklPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(KklInputMappingContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(KklInputMappingContext, 0);
+    
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+}
+
+void AKklPlayerController::SetupInputComponent()
+{
+    APlayerController::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AKklPlayerController::Move);
+}
+
+void AKklPlayerController::Move(const FInputActionValue& Value)
+{
+	const FVector2D InputActionVector = Value.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+    if(APawn* ControllerPawn = GetPawn<APawn>())
+	{
+		ControllerPawn->AddMovementInput(ForwardDirection, InputActionVector.Y);
+		ControllerPawn->AddMovementInput(RightDirection, InputActionVector.X);
+	}
+}
--- Source/Aura/Public/Player/KklPlayerController.h
+++ Source/Aura/Public/Player/KklPlayerController.h
@@ -1,10 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "KklPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  * 
@@ -13,5 +16,17 @@ UCLASS()
 class AURA_API AKklPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+		AKklPlayerController();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	UPROPERTY(EditAnywhere, Category ="Input")
+	TObjectPtr<UInputMappingContext> KklInputMappingContext;
 	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& Value);
 };
