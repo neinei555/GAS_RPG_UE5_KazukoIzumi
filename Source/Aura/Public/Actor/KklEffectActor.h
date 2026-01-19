ceb9478acd7ba3275f66b7406106013396afa480
KazukoLzumi <2126142969@qq.com>
2026-01-19 22:30:45 +0800

    mana,cue

--- Source/Aura/Public/Actor/KklEffectActor.h
+++ Source/Aura/Public/Actor/KklEffectActor.h
@@ -26,5 +26,6 @@ protected:
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
-
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 };
